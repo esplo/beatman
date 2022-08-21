da50ca744da63d0e26cb0c61153efe913149348c
esplo <esplo@users.noreply.github.com>
2022-08-21 15:19:13 +0900

    filter subdir in reconstruct

--- src/ops/reconstruct.rs
+++ src/ops/reconstruct.rs
@@ -7,6 +7,20 @@ use crate::chart_hashes::ChartHashes;
 use crate::errors::Result;
 use crate::fsutil;
 
+/// remove subdirectory to avoid moving into a parent directory
+fn filter_subdir(dir_paths: HashSet<&Path>) -> Result<HashSet<&Path>> {
+    let result = dir_paths
+        .clone()
+        .into_iter()
+        .filter(|d| {
+            !d.ancestors()
+                .filter(|pp| pp != d) // ignore myself
+                .any(|pp| dir_paths.contains(pp))
+        })
+        .collect();
+    Ok(result)
+}
+
 pub fn reconstruct(current_dir: &Path, dest_dir: &Path, dryrun: bool) -> Result<()> {
     let chart_hashes = ChartHashes::new(&current_dir)?;
     let parents: HashSet<&Path> = HashSet::from_iter(
@@ -16,6 +30,7 @@ pub fn reconstruct(current_dir: &Path, dest_dir: &Path, dryrun: bool) -> Result<
             .flat_map(|(_, v)| v[0].parent())
             .collect::<Vec<&Path>>(),
     );
+    let parents = filter_subdir(parents)?;
 
     for d in &parents {
         let mut move_to_dir = PathBuf::from(&dest_dir);
