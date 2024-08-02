ca444d2807cbcee236d5bb7dc0c0f578817ff03a
esplo <esplo@users.noreply.github.com>
2024-08-02 21:23:09 +0900

    use MD5 as hash string

--- src/ops/reconstruct.rs
+++ src/ops/reconstruct.rs
@@ -1,27 +1,32 @@
-use log::info;
-use std::collections::hash_map::DefaultHasher;
-use std::fs;
-use std::hash::{Hash, Hasher};
-use std::path::{Path, PathBuf};
-
 use crate::chart_hashes::ChartHashes;
 use crate::errors::Result;
 use crate::fsutil;
+use log::info;
+use md5::{Digest, Md5};
+use std::fs;
+use std::os::unix::ffi::OsStrExt;
+use std::path::{Path, PathBuf};
 
 pub fn reconstruct(current_dir: &Path, dest_dir: &Path, dryrun: bool, shard: bool) -> Result<()> {
-    let chart_hashes = ChartHashes::new(&current_dir)?;
+    let chart_hashes = ChartHashes::new(current_dir)?;
     let parents = chart_hashes.parents()?;
-    let mut hasher = DefaultHasher::new();
 
     for d in &parents {
         let mut move_to_dir = PathBuf::from(&dest_dir);
         let file_name = d.file_name().unwrap();
         if shard {
-            file_name.hash(&mut hasher);
-            let hash = hasher.finish() % 10;
-            move_to_dir.push(&Path::new(&hash.to_string()));
+            let mut hasher = Md5::new();
+            hasher.update(file_name.as_bytes());
+            let hash_bytes = hasher.finalize();
+
+            let hex_string: String = hash_bytes
+                .iter()
+                .map(|byte| format!("{:02X}", byte))
+                .collect();
+            // 先頭二文字をハッシュとする
+            move_to_dir.push(Path::new(&hex_string[0..2]));
         }
-        move_to_dir.push(&Path::new(file_name));
+        move_to_dir.push(Path::new(file_name));
         if d != &move_to_dir {
             info!("move: {:?} -> {:?}", d, move_to_dir);
 
